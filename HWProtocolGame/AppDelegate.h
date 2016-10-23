807fc15d9d1daa206060c972c69f06cdc98dfc17
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-10-23 23:40:09 +0300

    Initial Commit

---
+++ HWProtocolGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWProtocolGame
+//
+//  Created by Ildar Zalyalov on 23.10.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWProtocolGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWProtocolGame
+//
+//  Created by Ildar Zalyalov on 23.10.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
